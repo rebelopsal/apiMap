c71546fb482daa57c116f733de8252491d770c6e
Rebeca Lopez <rebelopezsaldias.rels@gmail.com>
2024-01-11 20:14:45 -0300

    map

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    const coord = {lat:-34.5453062 ,lng:-58.4497749}
+    const map = new google.maps.Map(document.getElementById("map"),
+    {zoom: 15,
+    center:coord});
+
+    const marker = new google.maps.Marker({
+        position: coord,
+        map : map
+    });
+}
